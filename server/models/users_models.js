4de0681d298355824aebcd662230fc5c7caa898d
Sheila Nkatha <catherinesheilankatha@gmail.com>
2022-02-03 16:56:24 +0300

    "Routes running"

--- server/models/users_models.js
+++ server/models/users_models.js
@@ -1,76 +1,34 @@
-const mongoose=require('mongoose')
-const validator=require('validator');
+const mongoose=require('mongoose');
 const bcrypt=require('bcrypt')
-const { Schema }=mongoose
-SALT=10
+
+
 
 const userSchema=mongoose.Schema({
     email:{
         type:String,
-        required:true,
         unique:true,
-        trim:true,
-        lowercase:true,
-        validate(value){
-            if(!validator.isEmail(value)){
-                throw new Error('Invalid email')
-            }
-            
-        }
+        require:true,
         
-    },
-    firstName:{
-        type:String,
-        trim:true,
-        maxLength:100,  
-        
-    },
-    lastName:{
-        type:String,
-        trim:true,
-        maxLength:100,
-       
-    },
-    age:{
-        type:Date,
-        default:Date.now
 
     },
     password:{
         type:String,
-        required:true,
-        trim:true
-      
-
+        require:true
     }
-   
-    
-},{timestamps:true});
-
-//function that runs before saving password to encrypt the password
-userSchema.pre('save',async function(next){
-    let user=this
-    //function runs only when changes are made to password
-    if (user.isModified('password')){
-        //create salt
-        const salt=await bcrypt.genSalt(10);
-          //hash pasword with created salt
-        const hash=await bcrypt.hash(user.password,salt)
-          //update the user.pasword in the db  
-        user.password=hash;
-    }
-    next();       
 })
-//compare password from input with password stored in db
-userSchema.methods.comparePassword=function(inputPassword,cb){
-    bcrypt.compare(inputPassword,this.password,(err,isMatch)=>{
-        if(err)return cb(err);
-        cb(null,isMatch);
-    })
-}
 
+userSchema.pre('save',async function(next){
+    let user=this;
+    if(user.isModified('password'))
+    {
+         const salt= await bcrypt.genSalt(10);
+         const hash=await bcrypt.hash(user.password,salt)
+         user.password=hash
 
+    }
+    next()
+})
 
-//convert schema definition to model one can work with
 const User=mongoose.model("User",userSchema)
 module.exports={ User }
+
--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -1,29 +1,41 @@
-const express=require('express');
-
+const express=require("express")
 let router=express.Router()
-require('dotenv').config();
 
-const{ User }=require("../../models/users_models")
+const { User }=require("../../models/users_models")
 
 router.route("/register")
-.post( async (req,res)=>{
+.post(async(req,res)=>{
    try{
-   const user=new User({
-     email:req.body.email,
-     password:req.body.password})
-   const  doc= await user.save()
+    //createing the mmodel
+    const user=new User({
+        email:req.body.email,
+        password:req.body.password
+    })
 
-   res.cookie('acept-cooke','hhgg')
-   .status(200).send(doc)
+    const doc = await user.save()
+    res.status(200).send(getUserProps(doc))
+}catch(error){
+    res.status(400).json({mesage:'Error',error:error})
+}
 
- 
+})
+router.route("/signin")
+.post(async(req,res)=>{
+    try{
 
-   }catch(error){
-     res.status(400).json({  message:"error"})
-
-   }
+    }catch(error){
+        
+    }
 })
 
 
+
+const getUserProps=(props)=>{
+    return{
+        _id:props._id,
+        email:props.email,
+        password:props.password
+    }
+}
 
 module.exports=router
